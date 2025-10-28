1a7f95b4382db7b7a5d679f963795363038a6a14
Vee C <vee@chorus.one>
2025-10-29 00:11:27 +0900

    cli: Ensure compatibility with [staking] section
    Ensure backwards compatibility with `[staking]` section and
    `funded_address_private_key` field so that users don't have to update
    their configuration when upgrading.

--- staking-cli/src/signer.py
+++ staking-cli/src/signer.py
@@ -8,13 +8,29 @@ def create_signer(config: dict) -> Signer:
     log = init_logging(config["log_level"].upper())
     signer_type = os.environ.get("SIGNER_TYPE")
     if not signer_type:
-        signer_type = config["signer"].get("type", "local").lower()
+        if config.get("staking", {}).get("funded_address_private_key"):
+            signer_type = "legacy"
+        else:
+            signer_type = config["signer"].get("type", "local").lower()
     if not signer_type:
         raise ValueError(
             "signer_type is required as SIGNER_TYPE env var or in config file"
         )
 
-    if signer_type == "local":
+    # For backwards compatibility
+    if signer_type == "legacy":
+        log.debug(f"Initializing legacy signer")
+        private_key = os.environ.get("FUNDED_ACCOUNT_PRIVATE_KEY")
+        if not private_key:
+            private_key = config["staking"]["funded_address_private_key"]
+        if not private_key:
+            raise ValueError(
+                "private_key is required as FUNDED_ACCOUNT_PRIVATE_KEY env var or in config file"
+            )
+        log.debug(f"Legacy signer initialized")
+        return LocalSigner(private_key)
+
+    elif signer_type == "local":
         log.debug(f"Initializing local signer")
         private_key = os.environ.get("PRIVATE_KEY")
         if not private_key:
